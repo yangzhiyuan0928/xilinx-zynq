7a878507fbe8016d475d7cd11460b72bf8adb66c
yangzhiyuan <yangzhiyuan0928@163.com>
2017-07-05 23:09:54 +0800

    Update axilite_reg.c

--- nau_signal_acquisition/linux/driver/axilite_reg.c
+++ nau_signal_acquisition/linux/driver/axilite_reg.c
@@ -28,7 +28,7 @@ int axilite_reg_fasync(int fd,struct file *filep,int mode)
 
     printk("driver test_fasync fuction, fasync_helper fuction\n");
 
-    retval=fasync_helper(fd,filep,mode,&xdev->async_queue);  //初始化/释放fasync_struct结构体
+    retval=fasync_helper(fd,filep,mode,&xdev->async_queue);  
                                                        
     if(retval<0) {                                             
        printk( "the retval is %d !\n",retval);                   
@@ -136,7 +136,7 @@ static const struct file_operations axilite_reg_fileops = {
 	//.read = axilite_reg_read,     
 	//.write = adc_write, 
 	.unlocked_ioctl = axilite_reg_ioctl,
-	.release = axilite_reg_release,  //当打开最后一个设备的用户进程执行close后，内核将调用驱动该函数，清理未完成的输入输出操作，释放资源等
+	.release = axilite_reg_release,  
 	.open = axilite_reg_open,
 	.owner = THIS_MODULE,
 	.fasync = axilite_reg_fasync,
@@ -149,7 +149,7 @@ static irqreturn_t axilite_reg_interrupt_handler(int irq, void *devid)
   printk("interrupt handler function! irq = %d\n",irq);
   if(xdev->async_queue)  
   {
-       	kill_fasync(&xdev->async_queue,SIGIO,POLL_IN);  //发送信号SIGIO给fasync_struct结构体所描述PID，触发应用层序的SIGIO信号处理函数
+       	kill_fasync(&xdev->async_queue,SIGIO,POLL_IN);  
   }
   else printk( "kill_fasync failed !\n");
 
@@ -174,7 +174,7 @@ static int axilite_reg_probe(struct platform_device *pdev)
 
 	printk("axilite_reg_probe start!!!\n"); 
 
-	xdev = devm_kzalloc(&pdev->dev, sizeof(*xdev), GFP_KERNEL);  //和设备相关的内核内存分配
+	xdev = devm_kzalloc(&pdev->dev, sizeof(*xdev), GFP_KERNEL);  
 	if (!xdev)
 		return -ENOMEM;
 
@@ -186,11 +186,11 @@ static int axilite_reg_probe(struct platform_device *pdev)
 	
 	/* Map the registers */
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	xdev->regs = devm_ioremap_resource(&pdev->dev, res);  //IO内存映射
+	xdev->regs = devm_ioremap_resource(&pdev->dev, res);  
 
 
 	/* find the IRQ line, if it exists in the device tree */
-	xdev->irq = irq_of_parse_and_map(node, 0);  //设备树里查找中断的描述项，然后返回中断号
+	xdev->irq = irq_of_parse_and_map(node, 0);  
 	status = request_irq(xdev->irq, axilite_reg_interrupt_handler,
 		  IRQF_TRIGGER_RISING | IRQF_SHARED, MODULE_NAME, xdev);  
 	if (status) {
@@ -199,18 +199,18 @@ static int axilite_reg_probe(struct platform_device *pdev)
 	}
 
 	status = alloc_chrdev_region(&xdev->devno, 
-	AXILITE_REG_MINOR_START, AXILITE_REG_MINOR_COUNT,MODULE_NAME);  //申请设备号
+	AXILITE_REG_MINOR_START, AXILITE_REG_MINOR_COUNT,MODULE_NAME);  
 	if (status < 0) {
 		dev_err(&pdev->dev, "unable to alloc chrdev \n");
 		return status;
 	}
 
 
-	cdev_init(&xdev->cdev, &axilite_reg_fileops);   //设备初始化 
+	cdev_init(&xdev->cdev, &axilite_reg_fileops);  
 	xdev->cdev.owner = THIS_MODULE; 
 	xdev->cdev.ops = &axilite_reg_fileops;
 
-	status = cdev_add(&xdev->cdev, xdev->devno,  //添加设备
+	status = cdev_add(&xdev->cdev, xdev->devno, 
 	         AXILITE_REG_MINOR_COUNT);
 	if (status){
 	   dev_err(&pdev->dev,"Err: failed in registering axilite_reg cdev.\n");
@@ -224,7 +224,7 @@ static int axilite_reg_probe(struct platform_device *pdev)
 	{
 		printk("failed creat  adc_class !\n");
 	}
-	devp = device_create( axilite_reg_class,NULL,xdev->devno,NULL,MODULE_NAME);	 //创建设备节点
+	devp = device_create( axilite_reg_class,NULL,xdev->devno,NULL,MODULE_NAME);	
 	if(IS_ERR(devp))	
 		printk("failed to creat device node!\n");
 
@@ -243,7 +243,7 @@ static int axilite_reg_remove(struct platform_device *pdev)
 
 	cdev_del(&xdev->cdev);
 	device_destroy(axilite_reg_class,MKDEV(MAJOR(xdev->devno), AXILITE_REG_MINOR_START));
-	unregister_chrdev_region(xdev->devno, AXILITE_REG_MINOR_COUNT);  //释放设备号
+	unregister_chrdev_region(xdev->devno, AXILITE_REG_MINOR_COUNT); 
 
 	class_destroy(axilite_reg_class);
 
@@ -256,7 +256,7 @@ static struct platform_driver axilite_reg_driver = {
 	.driver = {
 		.name = MODULE_NAME,     
 		.owner = THIS_MODULE,
-		.of_match_table = axilite_reg_of_match_table,  //设备描述，设备树文件匹配
+		.of_match_table = axilite_reg_of_match_table,  
 	},
 };
 module_platform_driver(axilite_reg_driver);
